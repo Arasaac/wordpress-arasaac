8a1e9b94e2046dfb29604a80d8fd0447a8ce65ad
juanda99 <juandacorreo@gmail.com>
2015-05-29 15:54:07 +0200

    Comenzada interfaz admin materiales y programas

--- web/wp-content/themes/enfold-child/functions.php
+++ web/wp-content/themes/enfold-child/functions.php
@@ -11,3 +11,37 @@ function after_head_image_func(){
     
 }
 add_action('ava_main_header', 'after_head_image_func');
+
+
+add_action('acf/save_post', 'my_save_post');
+
+function my_save_post( $post_id ) {
+	
+	// bail early if not a contact_form post
+	if( get_post_type($post_id) !== 'materiales' ) {
+		
+		return;
+		
+	}
+	
+		
+	// vars
+	$post = get_post( $post_id );
+	
+	
+	// get custom fields (field group exists for content_form)
+	$name = get_field('autor', $post_id);
+	$email = get_field('correo_electronico', $post_id);
+	
+	
+	// email data
+	$to = 'juandacorreo@gmail.com';
+	$headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+	$subject = $post->post_title;
+	$body = $post->post_content;
+	
+	
+	// send email
+	wp_mail($to, $subject, $body, $headers );
+	
+}
---
+++ web/wp-content/themes/enfold-child/template-programas.php
@@ -0,0 +1,66 @@
+<?php 
+	/*
+	Template Name: Template para programas
+	*/
+
+	 global $avia_config, $more;
+	 acf_form_head();
+	 get_header();
+	 echo avia_title();
+
+	 ?>
+
+
+
+		<div class='container_wrap container_wrap_first main_color <?php avia_layout_class( 'main' ); ?>'>
+
+			<div class='container'>
+
+
+<div id="content">
+	
+	<?php
+
+acf_form(array(
+		'post_id'		=> 'new_post',
+		'post_title'	=> true,
+		'post_content'	=> true,
+		'field_groups'       => array(99),
+		'new_post'		=> array(
+			'post_type'		=> 'programa',
+			'post_status'	=> 'publish'
+		),
+		/*'return'		=> home_url('contact-form-thank-you'),*/
+		'submit_value'	=> 'Enviar'
+	));
+
+	
+/*
+
+	$new_post = array(
+		'post_id'            => 'new', // Create a new post
+		// PUT IN YOUR OWN FIELD GROUP ID(s)
+		'field_groups'       => array(53), // Create post field group ID(s)
+		'form'               => true,
+		'return'             => '%post_url%', // Redirect to new post url
+		'html_before_fields' => '',
+		'html_after_fields'  => '',
+		'submit_value'       => 'Submit Post',
+		'updated_message'    => 'Saved!'
+	);
+	acf_form( $new_post );
+*/
+
+	
+	?>
+	
+</div>
+
+			</div><!--end container-->
+
+		</div><!-- close default .container_wrap element -->
+
+
+
+
+<?php get_footer(); ?>
